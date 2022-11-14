14baf3b8ff661571fb02facb70f445aec833a4fa
manningt <tom@manningetal.com>
2022-11-14 11:51:59 -0500

    add sync'ing to spkr

--- sync_repos.sh
+++ sync_repos.sh
@@ -5,15 +5,19 @@
 user_id="pi"
 dst_dir="/home/${user_id}/repos"
 src_dir="${dst_dir}/boomer_supporting_files"
+audio_dir="${dst_dir}/audio"
 
 # rsync -avhe ssh --del --exclude='.git/' --exclude='reference_files/' --dry-run $src_dir left:$dst_dir
 rsync -avhe ssh --del --exclude='.git/' --exclude='reference_files/' $src_dir left:$dst_dir
 rsync -avhe ssh --del --exclude='.git/' --exclude='reference_files/' $src_dir right:$dst_dir
+rsync -avhe ssh --del --exclude='.git/' --exclude='reference_files/' $src_dir spkr:$dst_dir
+
+rsync -avhe ssh --del --exclude='.git/' $audio_dir spkr:$dst_dir
 
 printf "\n\n -- Now issue the following commands:\n"
 printf "ssh left\n"
-printf "incrontab ${repo_dir}/incrontab.txt\n"
+printf "incrontab ${src_dir}/incrontab.txt\n"
 printf "exit\n"
 printf "ssh right\n"
-printf "incrontab ${repo_dir}/incrontab.txt\n"
+printf "incrontab ${src_dir}/incrontab.txt\n"
 printf "exit\n"

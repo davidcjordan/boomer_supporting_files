a1794740b6f9cdebca4e9f90248c9991dc31974e
manningt <tom@manningetal.com>
2023-02-25 10:05:39 -0500

    fix not connected handling

--- bt_audio_enable.sh
+++ bt_audio_enable.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # checks for bluetooth paired device, connects to it and write to file
+# if there are no paired devices, then it deletes the file to indicate that condition
 
 if [[ $1 ]]; then
  sleep_duration=$1
@@ -11,19 +12,30 @@ printf "bt_audio_enable.sh started: sleep_duration=$sleep_duration arg2=$2\n" >&
 
 filepath="/run/shm/bt_speaker.fifo"
 while true; do
-  bt_device_id=$(bluetoothctl devices | awk ' { print $2 } ')
+  bt_device_id=$(bluetoothctl paired-devices | awk ' { print $2 } ')
   # test if string is not empty
   if [[ $bt_device_id ]]; then
     # printf "paired with '$bt_device_id'\n"
     bluetoothctl trust $bt_device_id > /dev/null
     if [ $? -eq 0 ]; then
       # printf "trusted '$bt_device_id'\n"
-      bluetoothctl connect $bt_device_id > /dev/null
-      if [ $? -eq 0 ]; then
-        # printf "connected '$bt_device_id'\n"
-        rssi=$(hcitool rssi $bt_device_id | awk ' { print $4 } ')
-        # printf "rssi= $rssi\n"
+      conn_status=$(bluetoothctl info $bt_device_id | grep Connected | awk ' { print $2 } ')
+      if [[ -z $conn_status ]]; then
+        printf "bogus return from get info for '$bt_device_id'\n"
+      elif [[ "$conn_status" == "no" ]]; then
+        bluetoothctl connect $bt_device_id > /dev/null
+        conn_status=$(bluetoothctl info $bt_device_id | grep Connected | awk ' { print $2 } ')
+      fi
+      # at this point, should either be connected or not
+      if [[ "$conn_status" == "no" ]]; then
+        printf "$bt_device_id 0" > $filepath
+      else
+        rssi_string=$(hcitool rssi $bt_device_id)
+        # hcitool has an exit code of 1 if the device is not connected
         if [ $? -eq 0 ]; then
+          # printf "rssi_string= '$rssi_string'\n"
+          rssi=$(echo $rssi_string | awk ' { print $4 } ')
+          # printf "rssi= $rssi\n"
           printf "$bt_device_id $rssi" > $filepath
         else
           printf "$bt_device_id 0" > $filepath
